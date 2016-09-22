fc17d4fc8b90fba9d586961826753bc41b44ba72
Si Te Feng <fengsite@hotmail.com>
2016-09-22 16:28:57 -0400

    Added new projects and report

--- js/main.js
+++ js/main.js
@@ -57,25 +57,25 @@
 
 	var projects = [
 		{
-			"title": "Voxel",
-			"subtitle": "Lightpainting Magic Wand",
+			"title": "Dojo",
+			"subtitle": "iPhone App",
 			"details": [
-				"A wifi enabled and modular LED stick that connects to a smartphone. Gives photographers an innovative tool to create lightpainting at night with ease.",
-				"Collaborating 3 co-founders on this long-term project aimed towards mass production and profitability",
-				"Responsible for mechanical design, embedded programming, iOS development, and product management",
-				"(Dec, 2015 - Present)"
+				"Language learning and translation gamified",
+				"Using iPhone camera, Clarifai machine learning API, and Google Translate API to help users to learn new languages",
+				"PennApps fall 2016 Best UI/UX Award by Goldman Sachs",
+				"(September 9, 2016)"
 			],
-			"sourceURL": "https://github.com/sitefeng/Voxel",
-			"iconType": "icon-voxel",
-			"imageLinks": ["pImg/voxel1.jpg", "pImg/voxel2.jpg","pImg/voxel3.png","pImg/voxel4.jpg","pImg/voxel5.png","pImg/voxel6.png"]
-		},
+			"sourceURL": "https://github.com/sitefeng/dojo",
+			"iconType": "icon-dojo",
+			"imageLinks": ["pImg/dojo1.png", "pImg/dojo2.png", "pImg/dojo3.png", "pImg/dojo4.png", "pImg/dojo5.png", "pImg/dojo6.png"]
+		} , 
 		{
 			"title": "Relay",
 			"subtitle": "Sign in with your Fingerprint",
 			"details": [
+				"Grand-prize winner of MHacks 2015 at University of Michigan",
 				"Sign in to all social websites with iPhone TouchID",
 				"Using Chrome Extension, Firebase, and Native iOS app",
-				"Grand-prize winner of MHacks 2015 at University of Michigan",
 				"(Sep, 2015)"
 			],
 			"appURL": "http://devpost.com/software/relay-4o05pb",
@@ -83,10 +83,24 @@
 			"imageLinks": ["pImg/relay1.jpg", "pImg/relay2.jpg","pImg/relay3.jpg","pImg/relay4.png","pImg/relay5.png"]
 		},
 		{
+			"title": "MetroArm",
+			"subtitle": "Arduino + Leap Motion Robotic Arm",
+			"details": [
+				"Grand-prize winner of TreeHacks 2015 at Stanford University",
+				"Designed and hand-made the 6-DOF robotic arm at a local machine shop",
+				"Records hand position using Leap Motion sensor and sends the actions to the robotic arm through an Arduino Uno (C, Python)",
+				"(Feb, 2015)"
+			],
+			"appURL": "http://challengepost.com/software/metroarm",
+			"sourceURL": "https://github.com/ericye16/LeapControlledRobot",
+			"iconType": "icon-robot",
+			"imageLinks": ["pImg/metroArm6.jpg", "pImg/metroArm1.jpg","pImg/metroArm2.jpg","pImg/metroArm3.jpg","pImg/metroArm4.jpg","pImg/metroArm5.jpg"]
+		},
+		{
 			"title": "Hack the North",
 			"subtitle": "iPhone App",
 			"details": [
-				"Designed, built, and released the iOS app for Hack the North at University of Waterloo for the last two years",
+				"Designed, built, and released the iOS app for Hack the North at University of Waterloo",
 				"Collaborated with the Android team to come up with the feature specifications",
 				"Rebuilt the app in Swift for 2015 version with the new 'Uber for Mentor' feature, which allows hackers to conveniently request a person to help them on demand",
 				"2014:(July, 2014 - Sep, 2014), 2015:(May, 2015 - Sep, 2015)"
@@ -96,18 +110,45 @@
 			"imageLinks": ["pImg/hacka.png", "pImg/hackb.png", "pImg/hackc.png", "pImg/hackd.png", "pImg/hacke.png", "pImg/hack1.png", "pImg/hack2.png", "pImg/hack3.png", "pImg/hack4.png", "pImg/hack5.png", "pImg/hack6.png", "pImg/hack7.png"]
 		}, 
 		{
-			"title": "MetroArm",
-			"subtitle": "Arduino + Leap Motion Robotic Arm",
+			"title": "Uniq",
+			"subtitle": "iOS Universal App",
 			"details": [
-				"Designed and hand-made the 6-DOF robotic arm at a local machine shop",
-				"Records hand position using Leap Motion sensor and sends the actions to the robotic arm through an Arduino Uno (C, Python)",
-				"Grand-prize winner of TreeHacks 2015 at Stanford University",
-				"(Feb, 2015)"
+				"A college information assistant for high school students on mobile devices with integration to custom backend",
+				"Dramatically reduces the time spent on college research by condensing program information from various colleges into one unified app, so that the data is both personalized and easy to access",
+				"Responsible for iOS development in Objective-C, Swift, and business planning",
+				"(Nov, 2013 – Nov, 2014)"
 			],
-			"appURL": "http://challengepost.com/software/metroarm",
-			"sourceURL": "https://github.com/ericye16/LeapControlledRobot",
-			"iconType": "icon-robot",
-			"imageLinks": ["pImg/metroArm6.jpg", "pImg/metroArm1.jpg","pImg/metroArm2.jpg","pImg/metroArm3.jpg","pImg/metroArm4.jpg","pImg/metroArm5.jpg"]
+			"sourceURL": "https://github.com/sitefeng/Uniq-iOS",
+			"appURL": "https://itunes.apple.com/us/app/uniq-college-info-reimagined/id1143949288?mt=8",
+			"iconType": "icon-uniq",
+			"imageLinks": ["pImg/uniq1.png", "pImg/uniq2.png", "pImg/uniq3.png", "pImg/uniq4.png"]
+		},
+		{
+			"title": "Voxel",
+			"subtitle": "Lightpainting Magic Wand",
+			"details": [
+				"A wifi enabled and modular LED stick that connects to a smartphone. Gives photographers an innovative tool to create lightpainting at night with ease.",
+				"Collaborating 3 co-founders on this long-term project aimed towards mass production and profitability",
+				"Responsible for mechanical design, embedded programming, iOS development, and product management",
+				"(Dec, 2015 - June, 2016)"
+			],
+			"sourceURL": "https://github.com/sitefeng/Voxel",
+			"iconType": "icon-voxel",
+			"imageLinks": ["pImg/voxel1.jpg", "pImg/voxel2.jpg","pImg/voxel3.png","pImg/voxel4.jpg","pImg/voxel5.png","pImg/voxel6.png"]
+		},
+		{
+			"title": "Space 7",
+			"subtitle": "iPhone Game",
+			"details": [
+				"An open source Cocos2D iOS game (Objective-C)",
+				"Use touchscreen joystick and button to control a spaceship and shoot to destroy incoming asteroids",
+				"Two game modes with static velocity control and acceleration based control",
+				"(Oct, 2013 - Nov, 2013)"
+			],
+			"sourceURL": "https://github.com/sitefeng/Space7",
+			"appURL": "https://itunes.apple.com/us/app/space-7-free!/id738227589?mt=8",
+			"iconType": "icon-space7",
+			"imageLinks": ["pImg/space1.png", "pImg/space2.png", "pImg/space3.png", "pImg/space4.png", "pImg/space5.png"]
 		},
 		{
 			"title": "Fusion Smartstrap",
@@ -122,33 +163,6 @@
 			"iconType": "icon-fusion",
 			"imageLinks": ["pImg/fusion1.png","pImg/fusion2.png","pImg/fusion3.png","pImg/fusion4.png","pImg/fusion5.png"]
 		}, 
-		{
-			"title": "Uniq",
-			"subtitle": "iOS Universal App",
-			"details": [
-				"Co-founded Uniq, a college information assistant for high school students on mobile devices with integration to custom backend",
-				"Dramatically reduces the time spent on college research by condensing program information from various colleges into one unified app, so that the data is both personalized and easy to access",
-				"Responsible for iOS development in Objective-C, Swift with business planning on the side",
-				"(Nov, 2013 – Nov, 2014)"
-			],
-			"sourceURL": "https://github.com/sitefeng/Uniq-iOS",
-			"appURL": "http://www.sitefeng.info/uniq.html",
-			"iconType": "icon-uniq",
-			"imageLinks": ["pImg/uniq1.png", "pImg/uniq2.png", "pImg/uniq3.png", "pImg/uniq4.png"]
-		},
-		{
-			"title": "Space 7",
-			"subtitle": "iPhone Game",
-			"details": [
-				"An open source Cocos2D iOS game (Objective-C)",
-				"Use touchscreen joystick and button to control a spaceship and shoot to destroy incoming asteroids",
-				"Two game modes with static velocity control and acceleration based control",
-				"(Oct, 2013 - Nov, 2013)"
-			],
-			"sourceURL": "https://github.com/sitefeng/Space7",
-			"iconType": "icon-space7",
-			"imageLinks": ["pImg/space1.png", "pImg/space2.png", "pImg/space3.png", "pImg/space4.png", "pImg/space5.png"]
-		},
 		{
 			"title": "ArcTime",
 			"subtitle": "Watchface for Pebble Time",
@@ -170,14 +184,19 @@
 		"pdfLink": "https://medium.com/@sitefeng",
 		},
 		{
-		"title": "Report for Functional Reactive Programming and MVVM",
-		"date": "May 1, 2015",
+		"title": "PaperlessAI Prototype",
+		"date": "Sep 19, 2016",
 		"pdfLink": "pdfs/report1.pdf",
 		},
 		{
-		"title": "Report for Multi-layered View Hierarchy",
-		"date": "Sep 15, 2014",
+		"title": "iOS FRP and MVVM",
+		"date": "May 1, 2015",
 		"pdfLink": "pdfs/report2.pdf",
+		},
+		{
+		"title": "User Interface Elements with Multi-layered View Hierarchy",
+		"date": "Sep 15, 2014",
+		"pdfLink": "pdfs/report3.pdf",
 		}
 	];
 
